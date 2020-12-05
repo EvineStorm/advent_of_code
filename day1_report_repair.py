84971efb46c799b36958789812e63ae7be78756e
Evine Storm <evinestorm@gmail.com>
2020-12-05 18:39:42 +0100

    Finished the first half of the puzzle!

--- day1_report_repair.py
+++ day1_report_repair.py
@@ -5,3 +5,15 @@ with open("adventofcode_day1.txt") as day1_file:
     expense_report = [int(i) for i in expense_report]
     day1_file.close()
 
+for index, entry in enumerate(expense_report):
+    for compare_entry in expense_report[(index+1):]:
+        # prevents the numbers already added together on previous iterations being checked again
+        if entry + compare_entry == 2020:
+            print("The answer is: " + str(entry) + " * " + str(compare_entry) + " = "
+                  + str(entry * compare_entry) + " !")
+        else:
+            pass
+
+# In your expense report, what is the product of the three entries that sum to 2020?
+
+
